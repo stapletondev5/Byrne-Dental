c55964d86ff7ab67b02044e175bbd5456478b5b5
Daniel Pollock <danielstapleton@daniels-mbp.gateway.sonic.net>
2018-02-12 15:42:11 -0800

    Rougly got clients testimonials page done

--- script/carousel.js
+++ script/carousel.js
@@ -1,7 +1,13 @@
-$(document).ready(function () {
-    $('.carousel').carousel({
-        interval: 100000
-            });
-
-    $('.carousel').carousel('cycle');
+$(document).ready(function(){
+    $("#testimonial-slider").owlCarousel({
+        items:3,
+        itemsDesktop:[1000,3],
+        itemsDesktopSmall:[980,2],
+        itemsTablet:[768,2],
+        itemsMobile:[650,1],
+        pagination:true,
+        navigation:false,
+        slideSpeed:1000,
+        autoPlay:true
+    });
 });
